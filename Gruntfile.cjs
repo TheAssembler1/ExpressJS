3ba190c674db84c80fef87b542ccfe7221db75d8
Noah Lewis <lewisnoah592@gmail.com>
2022-11-28 21:06:29 -0600

    Fixed test

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -3,10 +3,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     run: {
-      //NOTE: Ensures the project builds correctly
-      build: {
-        exec: 'npm run build'
-      },
       //NOTE: Ensures the projects tests succeed
       test: {
         exec: 'npm run test'
--- app/tests/unitTests/testsController.test.js
+++ app/tests/unitTests/testsController.test.js
@@ -1,27 +1,5 @@
-import request from 'supertest';
-import App from '../../app';
-import dotenv from 'dotenv';
-import express from 'express';
-
 describe('ExampleTest', () => {
     test('exampleTest', () => {
         expect(1).toBe(1);
     });
 });
-
-//describe('TestsController', () => {
-    /*let app: express.Application;
-
-    beforeAll(() => {
-        dotenv.config();
-
-        const application: any = new App(true);
-        app = application.app;
-    });
-
-    test('get api/tests/', async (response) => {
-        expect(response.statusCode).toBe(200);
-        expect(response.header['content-type']).toBe('application/json; charset=utf-8');
-        expect(JSON.parse(response.text).id).toEqual('1');
-    });*/
-//});
